da7b855b632cba13b96eb625eee60f7f02912a7e
hbking00 <harshkumar10522@gmail.com>
2025-07-31 12:14:19 +0530

    databasecommit

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
-const DB_NAME = "jotly";
+const DB_NAME = "Blogbook";
 
-
-const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
+const connectDB=async()=>{
+    try{
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDB connected !! DB HOST:${connectionInstance.connection.host}`);
+        }
+    catch(error){
+        console.error("Error connecting to MongoDB:", error);
+        
     }
 }
-
-export default connectDB
+export default connectDB;
